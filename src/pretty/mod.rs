26dc8eeff49d5120d8220c46c18be887d71847c7
mgt <mgt@oi-wiki.org>
2024-05-26 12:44:33 +0800

    feat: allow single item code block be put in a single line

--- src/pretty/mod.rs
+++ src/pretty/mod.rs
@@ -429,7 +429,7 @@ impl PrettyPrinter {
             BoxDoc::nil(),
             (BoxDoc::text("{"), BoxDoc::text("}")),
             true,
-            FoldStyle::Never,
+            FoldStyle::Single,
         );
         doc
     }
